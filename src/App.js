07922315a37688d5d249a2daaba0af1dfcf14c2b
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-19 09:25:59 +0000

    about section file setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import Header from './Components/Header/Header';
+import About from './Components/About/About'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Header />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css';
+
+function About() {
+    return (
+        <>
+        <h3>About</h3>
+        </>
+    )
+    
+}
+
+export default About;
