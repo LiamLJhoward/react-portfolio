7f888f24c9f103a5f1954b7de2e40907c071c50d
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-22 10:36:39 +0000

    added avatar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Home.css';
+import ProfilePicture from './Creator.svg'
 
 export default function Home() {
     return (
-    <div>
-    <h1 className="header-title">Liam Howard </h1>
+    <div className="header-container">
+        <img src={ProfilePicture} alt="Liam Howard" width={500} height={500}></img>
+        <h1 className="header-title">Liam Howard </h1>
     </div>
     )
 }
